c0631dd111e6f3b1a0f4ac644930b1484647c0f4
Fracas <Fracas142@gmail.com>
2018-07-10 18:41:24 +0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,5 +3,5 @@ sudo apt-get install default-jdk
 sudo apt-get update
 mkdir minecraft
 cd minecraft
-wget -O minecraft_server.jar https://s3.amazonaws.com/Minecraft.Download/versions/1.7.4/minecraft_server.1.7.4.jar
+wget -O minecraft_server.jar https://s3.amazonaws.com/Minecraft.Download/versions/1.12.2/minecraft_server.1.12.2.jar
 java -jar minecraft_server.jar nogui
