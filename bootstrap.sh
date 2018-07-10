e9bf9b5302b575868b0148f7542e8b95a34bf851
Fracas <Fracas142@gmail.com>
2018-07-10 19:59:51 +0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,4 +4,4 @@ sudo apt-get update
 mkdir minecraft
 cd minecraft
 wget -O minecraft_server.jar https://s3.amazonaws.com/Minecraft.Download/versions/1.12.2/minecraft_server.1.12.2.jar
-java -jar minecraft_server.jar nogui
+wget -O start.sh https://raw.githubusercontent.com/BeTheWind42/minecraft/master/start.sh
