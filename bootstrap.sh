c249ae9e439039afb04509e270025bb2edf2c604
Fracas <Fracas142@gmail.com>
2018-07-10 20:14:06 +0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,5 +3,5 @@ sudo apt-get install default-jdk
 sudo apt-get update
 mkdir minecraft
 cd minecraft
-wget -O minecraft_server.jar https://s3.amazonaws.com/Minecraft.Download/versions/1.12.2/minecraft_server.1.12.2.jar
+wget -O minecraft_server.jar https://files.minecraftforge.net/maven/net/minecraftforge/forge/1.12.2-14.23.4.2732/forge-1.12.2-14.23.4.2732-installer.jar
 wget -O start.sh https://raw.githubusercontent.com/BeTheWind42/minecraft/master/start.sh
