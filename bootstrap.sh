f23f0d88a89ecae1895dd096d5e8c922f6847404
Fracas <Fracas142@gmail.com>
2018-07-10 18:39:59 +0200

    Create bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,7 @@
+
+sudo apt-get install default-jdk
+sudo apt-get update
+mkdir minecraft
+cd minecraft
+wget -O minecraft_server.jar https://s3.amazonaws.com/Minecraft.Download/versions/1.7.4/minecraft_server.1.7.4.jar
+java -Xmx1024M -Xms1024M -jar minecraft_server.jar nogui
