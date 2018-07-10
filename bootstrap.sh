67dffa97ab793ea4a81f9fad5fa025799c0d21ea
Fracas <Fracas142@gmail.com>
2018-07-10 18:40:31 +0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,4 +4,4 @@ sudo apt-get update
 mkdir minecraft
 cd minecraft
 wget -O minecraft_server.jar https://s3.amazonaws.com/Minecraft.Download/versions/1.7.4/minecraft_server.1.7.4.jar
-java -Xmx1024M -Xms1024M -jar minecraft_server.jar nogui
+java -jar minecraft_server.jar nogui
