d53c2326f60c4919c637ae41f360537f80ed23ae
Fracas <Fracas142@gmail.com>
2018-07-11 20:44:55 +0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,10 +11,13 @@ cd mods
 wget -O Mantle.jar https://files.forgecdn.net/files/2550/550/Mantle-1.12-1.3.2.24.jar
 
 #Mods
-#wget -O Dungeon_Mobs.zip https://github.com/BeTheWind42/minecraft/raw/master/mods/Dungeon%20Mobs%20v3.0.5.zip
+
 wget -O Gravestone.jar https://files.forgecdn.net/files/2524/880/gravestone-1.10.1.jar
 wget -O Natura.jar https://files.forgecdn.net/files/2553/697/natura-1.12.2-4.3.2.49.jar
 wget -O RoguelikeDongeons.jar https://files.forgecdn.net/files/2492/586/RoguelikeDungeons-1.12.2-1.8.0.jar
 wget -O SwingThroughGrass.jar https://files.forgecdn.net/files/2508/268/stg-1.12.2-1.2.3.jar
-wget -O DimensionalDoors.jar https://files.forgecdn.net/files/2558/528/DimensionalDoors-3.0.9-287.jar
-wget -O TheBeneath https://files.forgecdn.net/files/2573/626/The%20Beneath-1.12.2-1.4.2.jar
+wget -O ChunkPregenerator.jar https://files.forgecdn.net/files/2572/939/Chunk%20Pregenerator%20V1.12-1.8.1.jar
+wget -O RecurrentComplex.jar https://files.forgecdn.net/files/2516/761/RecurrentComplex-1.4.7.jar
+
+#wget -O DimensionalDoors.jar https://files.forgecdn.net/files/2558/528/DimensionalDoors-3.0.9-287.jar
+#wget -O TheBeneath.jar https://files.forgecdn.net/files/2573/626/The%20Beneath-1.12.2-1.4.2.jar
