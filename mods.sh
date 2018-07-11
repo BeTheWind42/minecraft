af3e60ae066528c3e0ecc8f8d0ed40aba918cb32
Fracas <Fracas142@gmail.com>
2018-07-11 20:48:56 +0200

    Create mods.sh

---
+++ mods.sh
@@ -0,0 +1,15 @@
+#Apis
+wget -O Mantle.jar https://files.forgecdn.net/files/2550/550/Mantle-1.12-1.3.2.24.jar
+wget -O IvToolkit.jar https://files.forgecdn.net/files/2443/253/IvToolkit-1.3.3-1.12.jar
+
+#Mods
+
+wget -O Gravestone.jar https://files.forgecdn.net/files/2524/880/gravestone-1.10.1.jar
+wget -O Natura.jar https://files.forgecdn.net/files/2553/697/natura-1.12.2-4.3.2.49.jar
+wget -O RoguelikeDongeons.jar https://files.forgecdn.net/files/2492/586/RoguelikeDungeons-1.12.2-1.8.0.jar
+wget -O SwingThroughGrass.jar https://files.forgecdn.net/files/2508/268/stg-1.12.2-1.2.3.jar
+wget -O ChunkPregenerator.jar https://files.forgecdn.net/files/2572/939/Chunk%20Pregenerator%20V1.12-1.8.1.jar
+wget -O RecurrentComplex.jar https://files.forgecdn.net/files/2516/761/RecurrentComplex-1.4.7.jar
+
+#wget -O DimensionalDoors.jar https://files.forgecdn.net/files/2558/528/DimensionalDoors-3.0.9-287.jar
+#wget -O TheBeneath.jar https://files.forgecdn.net/files/2573/626/The%20Beneath-1.12.2-1.4.2.jar
