1f9493f026da5195f8d57c7a2e6ef436fd645eef
Fracas <Fracas142@gmail.com>
2018-07-13 16:15:41 +0200

    Update mods.sh

--- mods.sh
+++ mods.sh
@@ -12,17 +12,17 @@ wget -O SilentLib.jar https://files.forgecdn.net/files/2545/98/SilentLib-1.12-2.
 wget -O iChunUtil.jar https://files.forgecdn.net/files/2510/760/iChunUtil-1.12.2-7.1.4.jar
 
 #Mods
-
 wget -O Gravestone.jar https://files.forgecdn.net/files/2524/880/gravestone-1.10.1.jar
 wget -O Natura.jar https://files.forgecdn.net/files/2553/697/natura-1.12.2-4.3.2.49.jar
 wget -O RoguelikeDongeons.jar https://files.forgecdn.net/files/2492/586/RoguelikeDungeons-1.12.2-1.8.0.jar
 wget -O SwingThroughGrass.jar https://files.forgecdn.net/files/2508/268/stg-1.12.2-1.2.3.jar
 wget -O ChunkPregenerator.jar https://files.forgecdn.net/files/2572/939/Chunk%20Pregenerator%20V1.12-1.8.1.jar
 wget -O RecurrentComplex.jar https://files.forgecdn.net/files/2516/761/RecurrentComplex-1.4.7.jar
-wget -O ScalingHealth.jar https://files.forgecdn.net/files/2576/434/ScalingHealth-1.12-1.3.19-102.jar
 wget -O LootBags.jar https://files.forgecdn.net/files/2508/87/LootBags-1.12.2-2.5.3.jar
 wget -O BackTools.jar https://files.forgecdn.net/files/2487/71/BackTools-1.12.2-7.0.0.jar
 wget -O Bloodmoon.jar https://files.forgecdn.net/files/2537/917/Bloodmoon-MC1.12.2-1.5.3.jar
+wget -O Baubles.jar https://files.forgecdn.net/files/2518/667/Baubles-1.12-1.5.2.jar
+wget -O baubley-heart-canisters.jar https://files.forgecdn.net/files/2563/927/baubley-heart-canisters-1.12.2-1.1.1.jar
 
 #wget -O DimensionalDoors.jar https://files.forgecdn.net/files/2558/528/DimensionalDoors-3.0.9-287.jar
 #wget -O TheBeneath.jar https://files.forgecdn.net/files/2573/626/The%20Beneath-1.12.2-1.4.2.jar
