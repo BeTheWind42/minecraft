ff8da12600f0f5d39f326d8aef7ad04e4cb4641c
Fracas <Fracas142@gmail.com>
2018-07-13 13:56:01 +0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,6 @@
 mkdir minecraft
 cd minecraft
 wget -O minecraftforge-installer.jar https://files.minecraftforge.net/maven/net/minecraftforge/forge/1.12.2-14.23.4.2705/forge-1.12.2-14.23.4.2705-installer.jar
-wget -O spongeforge.jar https://repo.spongepowered.org/maven/org/spongepowered/spongeforge/1.12.2-2705-7.1.0-BETA-3292/spongeforge-1.12.2-2705-7.1.0-BETA-3292.jar
 wget -O start.sh https://raw.githubusercontent.com/BeTheWind42/minecraft/master/start.sh
 wget -O server.properties https://raw.githubusercontent.com/BeTheWind42/minecraft/master/server.properties
 wget -O eula.txt https://raw.githubusercontent.com/BeTheWind42/minecraft/master/eula.txt
