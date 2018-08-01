f248a13f6bbbeab83f665a1850f5e346fd8c529f
Fracas <Fracas142@gmail.com>
2018-08-01 18:00:56 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-java -Xms4G -Xmx4G -jar forge-1.12.2-14.23.4.2705-universal.jar nogui
+java -server -Xms4G -Xmx4G -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -XX:+CMSIncrementalPacing -XX:ParallelGCThreads=2 -XX:+AggressiveOpts -jar forge-1.12.2-14.23.4.2705-universal.jar nogui
